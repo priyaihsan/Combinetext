aaa9641189c830b4852a4ca2905a23c11f67e9ee
priya ihsan <priyaihsan771@gmail.com>
2022-03-10 15:10:17 +0700

    Add project files.

---
+++ Combinetext/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Combinetext
+{
+    /// <summary>
+    /// returntext class
+    /// </summary>
+    /// <remarks>class returntext dapat menggabungkan dua kata menjadi satu</remarks>
+    class returntext
+    {
+        /// <summary>
+        /// operasi penggabungan kata
+        /// </summary>
+        /// <param name="a">kata pertama</param>
+        /// <param name="b">kata kedua</param>
+        /// <returns>hasil dari penggabungan antara kedua kata a dan b</returns>
+        public string text(string a, string b)
+        {
+            string x = a + " combine with " + b;
+            return x;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            returntext r = new returntext();
+            string output = r.text("banana", "apple");
+            Console.WriteLine(output);
+            Console.ReadLine();
+        }
+    }
+}
